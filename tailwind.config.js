6c6bc731db65cad5e5f6213030d372d37d770fa2
ekaspersen <eskilkasp@gmail.com>
2022-09-15 22:20:22 +0200

    everything

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,25 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
+        /*Colors | extend ->BackgroundImage*/
         colors: {
             primary_clr: {
-                DEFAULT: "#0C314A",
-                dark: "#081F31",
+                DEFAULT: "#081F31",
+                primaryDark: "#04101A",
+                menuBg: "#0F3A5C",
+                dark: "#000",
+                light: "#fff",
             },
             secondary: {
                 default: "#D4D0BA",
-            },
-            gradient: {
-                home_logo_shadow:
-                    "radial-gradient(50% 50% at 50% 50%, rgba(0, 0, 0, 0.66) 0%, rgba(0, 0, 0, 0) 100%)",
             },
         },
         extend: {
             backgroundImage: {
                 "gradient-radial":
                     "radial-gradient(50% 50% at 50% 50%, rgba(0, 0, 0, 0.66) 0%, rgba(0, 0, 0, 0) 100%)",
+                "fade-out-hero-gradient":
+                    "linear-gradient(90deg, #000 33.33%, rgba(0, 0, 0, 0) 100%);",
             },
         },
     },
